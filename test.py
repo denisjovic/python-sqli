c6abc4093defaed8710e08df494f695ff3152799
denisjovic <denisjovic@pm.me>
2021-11-11 13:11:15 +0100

    added new file

---
+++ test.py
@@ -0,0 +1,24 @@
+# Import module
+import sqlite3
+from flask import Flask, jsonify, render_template, request, g
+
+
+app = Flask(__name__)
+
+
+  
+# Connecting to sqlite
+conn = sqlite3.connect('gfg.db')
+cursor = conn.cursor()
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        user = request.form['username']
+        passw = request.form['password']
+        
+        cursor.execute("SELECT * FROM employees WHERE username = '%s' AND password = '%s'" %(user,passw))
+        conn.commit()
+        conn.close()
+
